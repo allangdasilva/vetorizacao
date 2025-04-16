8e3863bf2fe71da1aee187b1682e64787620d7b8
Allan Silva <allandasilva33@outlook.com>
2025-04-16 00:28:14 -0300

    Configurações inicias
    Organização dos arquivos + instalação tailwind

---
+++ src/Header.tsx
@@ -0,0 +1,20 @@
+const Header = () => {
+  return (
+    <>
+      <header>
+        <ul>
+          <li>
+            <a href="./" className="">
+              Allan Silva
+            </a>
+          </li>
+          <li>
+            <a href="#contato">Contato</a>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+};
+
+export default Header;
