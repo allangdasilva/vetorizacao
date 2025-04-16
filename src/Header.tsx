e55114ecb5524322792b27efe682968e7e6108e2
Allan Silva <allandasilva33@outlook.com>
2025-04-16 00:52:18 -0300

    Header
    Estrutura HTML completa e estilização finalizada

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,17 +1,21 @@
 const Header = () => {
   return (
     <>
-      <header>
-        <ul>
-          <li>
-            <a href="./" className="">
-              Allan Silva
-            </a>
-          </li>
-          <li>
-            <a href="#contato">Contato</a>
-          </li>
-        </ul>
+      <header className="bg-p6">
+        <nav className="container py-5">
+          <ul className="flex items-center justify-between flex-wrap gap-5 *:*:font-jost *:*:font-semibold *:*:block *:*:text-p7">
+            <li>
+              <a href="./" className="text-5xl leading-[1.12] ">
+                Allan Silva
+              </a>
+            </li>
+            <li>
+              <a href="#contato" className="text-[2rem] leading-[1.2]">
+                Contato
+              </a>
+            </li>
+          </ul>
+        </nav>
       </header>
     </>
   );
